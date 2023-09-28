98b07cd937f825a74d6a073787924c3f82957fed
mokybrow <mokybrow@gmail.com>
2023-09-28 17:41:27 +0200

    Добавил валидацию формы регистрации

--- frontend/src/components/RegForm.tsx
+++ frontend/src/components/RegForm.tsx
@@ -28,8 +28,18 @@ const RegForm: FC = () => {
         formState: { errors },
     } = useForm<FormData>()
 
-    const onSubmit: SubmitHandler<FormData> = (data) => console.log(data)
+    const onSubmit: SubmitHandler<FormData> = (data) => {
+        auth_store.registr(email,password, username, name )
+    }
     const error: SubmitErrorHandler<FormData> = (data) => console.log(data)
+
+    const uniqEmail = (email: string) => {
+        auth_store.registr(email, password, username, name)
+        if (!auth_store.emailError) {
+            return false
+        }
+        return true;
+    };
     const uniqUsername = (username: string) => {
         user_store.FindUser(username)
         if (user_store.anotherUser.username == username) {
@@ -43,14 +53,24 @@ const RegForm: FC = () => {
         <form onSubmit={handleSubmit(onSubmit, error)}>
             {/* register your input into the hook by invoking the "register" function */}
             <input  {...register("email", {
+                validate: uniqEmail, minLength: {
+                    value: 2,
+                    message: "min length is 5",
+                },
+            },)} onChange={e => setEmail(e.target.value)} placeholder='Email'/>
+            {errors.email && <p>Пользователь с такой почтой существует</p>}
+
+            <input  {...register("username", {
                 validate: uniqUsername, minLength: {
                     value: 2,
                     message: "min length is 5",
                 },
-            },)} />
+            },)} onChange={e => setUsername(e.target.value)}  placeholder='Username'/>
+
+            <input {...register("name", { required: true })} onChange={e => setName(e.target.value)} placeholder='Name'/>
 
             {/* include validation with required or other standard HTML validation rules */}
-            <input {...register("password", { required: true })} />
+            <input {...register("password", { required: true })} onChange={e => setPassword(e.target.value)} placeholder='Password'/>
             {/* errors will return when field validation fails  */}
             {errors.password && <span>This field is required</span>}
 
