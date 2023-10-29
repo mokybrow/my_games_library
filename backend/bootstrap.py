3e4ae688f1bc907191b7f4b3971b686da1b892ab
Mikhail Panin <mokybrow@gmail.com>
2023-10-29 10:37:58 +0200

    v1.0.3

--- backend/bootstrap.py
+++ backend/bootstrap.py
@@ -36,6 +36,7 @@ def make_app() -> FastAPI:
     origins = [
         'http://localhost:3000',
         'http://127.0.0.1:3000',
+        'http://0.0.0.0:3000',
         'http://frontend:3000',
         'cultofbear.ru',
         'http://cultofbear.ru',
