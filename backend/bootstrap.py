8dd53b490f1f36b4abd49f2efc515ed37b953dfc
Mikhail Panin <mokybrow@gmail.com>
2023-10-29 19:19:12 +0200

    v 1.0.10

--- backend/bootstrap.py
+++ backend/bootstrap.py
@@ -38,6 +38,8 @@ def make_app() -> FastAPI:
         'http://127.0.0.1:3000',
         'http://localhost:8000',
         'http://localhost',
+        'http://cultofbear.ru/',
+        'https://cultofbear.ru/'
     ]
 
     app.add_middleware(
